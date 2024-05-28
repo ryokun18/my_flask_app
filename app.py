d4c642bfc101e97e7b09abf42cd17d57a2550671
ryo <ryo@ryokun.local>
2024-05-28 21:51:20 +0900

    Update to use Google Sheets ID

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
+import json
 
 app = Flask(__name__)
 
@@ -14,15 +15,16 @@ def scrape_website(url):
     email_addresses = [element.text for element in soup.find_all('div', class_='email-address')]
     return company_names, email_addresses
 
-def authenticate_google_sheets(json_keyfile):
+def authenticate_google_sheets(json_key):
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name(json_keyfile, scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(json_key, scope)
     client = gspread.authorize(creds)
     return client
 
-def update_google_sheet(sheet_name, data):
-    client = authenticate_google_sheets('path_to_your_json_keyfile.json')
-    sheet = client.open(sheet_name).sheet1
+def update_google_sheet(sheet_id, data):
+    json_key = json.loads(os.environ.get('GOOGLE_SHEETS_JSON_KEY'))
+    client = authenticate_google_sheets(json_key)
+    sheet = client.open_by_key(sheet_id).sheet1
     for i, (company, email) in enumerate(data, start=2):
         sheet.update_cell(i, 1, company)
         sheet.update_cell(i, 2, email)
@@ -34,10 +36,10 @@ def index():
 @app.route('/scrape', methods=['POST'])
 def scrape():
     url = request.form['url']
-    sheet_name = request.form['sheet_name']
+    sheet_id = request.form['sheet_id']  # フォームの入力フィールド名を 'sheet_name' から 'sheet_id' に変更
     company_names, email_addresses = scrape_website(url)
     data = list(zip(company_names, email_addresses))
-    update_google_sheet(sheet_name, data)
+    update_google_sheet(sheet_id, data)
     return 'Data has been successfully scraped and updated in Google Sheet.'
 
 if __name__ == "__main__":
